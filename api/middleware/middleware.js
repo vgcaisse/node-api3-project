4eb8ee13511418a01a3877647f0100f2b2263e41
Visnu <vgcaisse@gmail.com>
2022-04-13 19:21:07 -0400

    logger is now working, starting to understand wat im doing... somewhat

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,35 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+
+  console.log(`logger middleware, ${timestamp}, ${method}, ${url}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`validateUserId middleware`)
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`validateUser middleware`)
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`validatePost middleware`)
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
